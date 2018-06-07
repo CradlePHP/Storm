946bc819593df774af27d5bc559edb799de604ec
aprilsacil <aprilvsacil@gmail.com>
2018-06-07 20:13:52 +0800

    adjusted checking for valid columns

--- src/Model.php
+++ src/Model.php
@@ -50,22 +50,22 @@ class Model extends DataModel
      * @const string TIMESTAMP Default timestamp format
      */
     const TIMESTAMP = 'U';
-    
+
     /**
      * @var string|null $table Table name
      */
     protected $table = null;
-    
+
     /**
      * @var SqlInterface|null $database Sql database object
      */
     protected $database = null;
-    
+
     /**
      * @var array $meta Stored table meta data
      */
     protected static $meta = [];
-    
+
     /**
      * Useful method for formating a time column.
      *
@@ -81,29 +81,29 @@ class Model extends DataModel
             //do nothing more
             return $this;
         }
-        
+
         if (is_numeric($this->data[$column])) {
             $this->data[$column] = (int) $this->data[$column];
         }
-        
+
         //if this is column is a string
         if (is_string($this->data[$column])) {
             //make into time
             $this->data[$column] = strtotime($this->data[$column]);
         }
-        
+
         //if this column is not an integer
         if (!is_int($this->data[$column])) {
             //do nothing more
             return $this;
         }
-        
+
         //set it
         $this->data[$column] = date($format, $this->data[$column]);
-        
+
         return $this;
     }
-    
+
     /**
      * Inserts model to database
      *
@@ -121,36 +121,36 @@ class Model extends DataModel
                 //throw error
                 throw SqlException::forTableNotSet();
             }
-            
+
             $table = $this->table;
         }
-        
+
         //if no database
         if (is_null($database)) {
             //and no default database
             if (!$this->database) {
                 throw SqlException::forDatabaseNotSet();
             }
-            
+
             $database = $this->database;
         }
-        
+
         //get the meta data, the valid column values and whether is primary is set
         $meta = $this->getMeta($table, $database);
         $data = $this->getValidColumns(array_keys($meta[self::COLUMNS]));
-        
+
         //we insert it
         $database->insertRow($table, $data);
-        
+
         //only if we have 1 primary key
         if (count($meta[self::PRIMARY]) == 1) {
             //set the primary key
             $this->data[$meta[self::PRIMARY][0]] = $database->getLastInsertedId();
         }
-        
+
         return $this;
     }
-    
+
     /**
      * Removes model from database
      *
@@ -172,32 +172,32 @@ class Model extends DataModel
                 //throw error
                 throw SqlException::forTableNotSet();
             }
-            
+
             $table = $this->table;
         }
-        
+
         //if no database
         if (is_null($database)) {
             //and no default database
             if (!$this->database) {
                 throw SqlException::forDatabaseNotSet();
             }
-            
+
             $database = $this->database;
         }
-        
+
         //get the meta data and valid columns
         $meta = $this->getMeta($table, $database);
         $data = $this->getValidColumns(array_keys($meta[self::COLUMNS]));
-        
+
         if (is_null($primary)) {
             $primary = $meta[self::PRIMARY];
         }
-        
+
         if (is_string($primary)) {
             $primary = [$primary];
         }
-        
+
         $filter = [];
         //for each primary key
         foreach ($primary as $column) {
@@ -207,17 +207,17 @@ class Model extends DataModel
                 //do nothing more
                 return $this;
             }
-            
+
             //add the condition to the filter
             $filter[] = [$column.'=%s', $data[$column]];
         }
-        
+
         //we delete it
         $database->deleteRows($table, $filter);
-        
+
         return $this;
     }
-    
+
     /**
      * Inserts or updates model to database
      *
@@ -239,41 +239,41 @@ class Model extends DataModel
                 //throw error
                 throw SqlException::forTableNotSet();
             }
-            
+
             $table = $this->table;
         }
-        
+
         //if no database
         if (is_null($database)) {
             //and no default database
             if (!$this->database) {
                 throw SqlException::forDatabaseNotSet();
             }
-            
+
             $database = $this->database;
         }
-        
+
         //get the meta data, the valid column values and whether is primary is set
         $meta = $this->getMeta($table, $database);
-        
+
         if (is_null($primary)) {
             $primary = $meta[self::PRIMARY];
         }
-        
+
         if (is_string($primary)) {
             $primary = [$primary];
         }
-        
+
         $primarySet = $this->isPrimarySet($primary);
-        
+
         //if no primary meta or primary values are not set
         if (empty($primary) || !$primarySet) {
             return $this->insert($table, $database);
         }
-        
+
         return $this->update($table, $database, $primary);
     }
-    
+
     /**
      * Sets the default database
      *
@@ -286,7 +286,7 @@ class Model extends DataModel
         $this->database = $database;
         return $this;
     }
-    
+
     /**
      * Sets the default database
      *
@@ -299,7 +299,7 @@ class Model extends DataModel
         $this->table  = $table;
         return $this;
     }
-    
+
     /**
      * Updates model to database
      *
@@ -321,51 +321,51 @@ class Model extends DataModel
                 //throw error
                 throw SqlException::forTableNotSet();
             }
-            
+
             $table = $this->table;
         }
-        
+
         //if no database
         if (is_null($database)) {
             //and no default database
             if (!$this->database) {
                 throw SqlException::forDatabaseNotSet();
             }
-            
+
             $database = $this->database;
         }
-        
+
         //get the meta data, the valid column values and whether is primary is set
         $meta = $this->getMeta($table, $database);
         $data = $this->getValidColumns(array_keys($meta[self::COLUMNS]));
-        
+
         //update original data
         $this->original = $this->data;
-        
+
         //from here it means that this table has primary
         //columns and all primary values are set
-        
+
         if (is_null($primary)) {
             $primary = $meta[self::PRIMARY];
         }
-        
+
         if (is_string($primary)) {
             $primary = [$primary];
         }
-        
+
         $filter = [];
         //for each primary key
         foreach ($primary as $column) {
             //add the condition to the filter
             $filter[] = [$column.'=%s', $data[$column]];
         }
-        
+
         //we update it
         $database->updateRows($table, $data, $filter);
-        
+
         return $this;
     }
-    
+
     /**
      * Checks to see if we have a primary value/s set
      *
@@ -380,10 +380,10 @@ class Model extends DataModel
                 return false;
             }
         }
-        
+
         return true;
     }
-    
+
     /**
      * Returns the table meta data
      *
@@ -398,9 +398,9 @@ class Model extends DataModel
         if (isset(self::$meta[$uid][$table])) {
             return self::$meta[$uid][$table];
         }
-        
+
         $columns = $database->getColumns($table);
-        
+
         $meta = [];
         foreach ($columns as $i => $column) {
             $meta[self::COLUMNS][$column['Field']] = [
@@ -409,17 +409,17 @@ class Model extends DataModel
                 'default'   => $column['Default'],
                 'empty'     => $column['Null'] == 'YES'
             ];
-            
+
             if ($column['Key'] == 'PRI') {
                 $meta[self::PRIMARY][] = $column['Field'];
             }
         }
-        
+
         self::$meta[$uid][$table] = $meta;
-        
+
         return $meta;
     }
-    
+
     /**
      * Returns only the valid data given
      * the partiular table
@@ -431,14 +431,16 @@ class Model extends DataModel
     protected function getValidColumns($columns)
     {
         $valid = [];
+        $dataColumns = array_keys($this->data);
+        
         foreach ($columns as $column) {
-            if (!isset($this->data[$column])) {
+            if (!in_array($column, $dataColumns)) {
                 continue;
             }
-            
+
             $valid[$column] = $this->data[$column];
         }
-        
+
         return $valid;
     }
 }
